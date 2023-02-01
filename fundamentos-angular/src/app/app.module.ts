54417d3c3e7cad01667ba4f4bdcd39af186b4cb7
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2023-01-31 22:58:03 -0300

    exemplos do angular

--- fundamentos-angular/src/app/app.module.ts
+++ fundamentos-angular/src/app/app.module.ts
@@ -1,12 +1,20 @@
+import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+import { MultiplicaPorPipe } from './multiplica-por.pipe';
+import { PipeCustomizadoComponent } from './pipe-customizado/pipe-customizado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponenteComponent,
+    SegundoComponenteComponent,
+    MultiplicaPorPipe,
+    PipeCustomizadoComponent
   ],
   imports: [
     BrowserModule,
---
+++ fundamentos-angular/src/app/segundo-componente/segundo-componente.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-segundo-componente',
+  templateUrl: './segundo-componente.component.html',
+  styleUrls: ['./segundo-componente.component.css']
+})
+export class SegundoComponenteComponent {
+
+  nome = "Vinicius";
+  dataNascimento = "1995-01-01";
+
+}
