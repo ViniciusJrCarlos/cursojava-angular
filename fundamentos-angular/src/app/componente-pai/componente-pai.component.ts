78941f5222445b9ddb13e5013ac6c0e31bcc9d64
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2023-04-18 00:57:42 -0300

    trabalhando com elementos e components

---
+++ fundamentos-angular/src/app/componente-pai/componente-pai.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-pai',
+  templateUrl: './componente-pai.component.html',
+  styleUrls: ['./componente-pai.component.css']
+})
+export class ComponentePaiComponent {
+
+  sobrenome = "da silva";
+
+
+mostraNomeCompleto(nomeCompleto: any){
+
+  alert(`O nome completo  é: ${nomeCompleto}`);
+
+}
+
+}
